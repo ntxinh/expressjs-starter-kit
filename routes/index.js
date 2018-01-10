10d6e4542ef907e826354b823501bfddd5d43bde
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2018-01-10 22:05:06 +0700

    Add comment for routes

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,14 @@ const { catchErrors } = require('../handlers/errorHandlers')
 const userController = require('../controllers/userController')
 const { getAuthorize } = require('../middlewares/authMiddleware')
 
+// Unprotected routes
 router.get('/', (req, res) => res.json({ msg: 'Hello world' }))
 router.post('/api/authenticate', catchErrors(userController.postAuthenticate))
 
 // Middlewares
 router.use(getAuthorize)
 
+// Protected routes
 router.get('/api/users', catchErrors(userController.getUsers))
 
 module.exports = router
