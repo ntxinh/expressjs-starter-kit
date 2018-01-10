9928cd32e2e7e7ed7f519d6edfe624117efdc0ce
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2018-01-10 21:44:23 +0700

    Add method verify jwt

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,23 +17,42 @@ exports.postAuthenticate = async (req, res) => {
 
     // check if user exist
     if (!user) {
-        res.json({ success: false, message: 'Authentication failed. User not found.' })
-        return
+        return res.json({ success: false, message: 'Authentication failed. User not found.' })
     }
 
     // check if password matches
     if (user.password != password) {
-        res.json({ success: false, message: 'Authentication failed. Wrong password.' })
-        return
+        return res.json({ success: false, message: 'Authentication failed. Wrong password.' })
     }
 
     // create a token with only our given payload
     let token = jwt.sign({ email: email }, process.env.JWT_SECRET, { expiresIn: '1h' })
 
     // return the information including token as JSON
-    res.json({
+    return res.json({
         success: true,
         message: 'Enjoy your token!',
         token: token
     })
+}
+
+exports.getAuthorize = async (req, res) => {
+
+    // check header or url parameters or post parameters for token
+    let token = req.body.token || req.query.token || req.headers['x-access-token']
+
+    // decode token
+    if (token) {
+
+        // verifies secret and checks exp
+        try {
+            let decoded = await jwt.verify(token, process.env.JWT_SECRET)
+            req.decoded = decoded
+            return res.json({ success: true, message: 'Enjoy your decoded!', decoded })
+        } catch (err) {
+            return res.json({ success: false, message: 'Failed to authenticate token.', error: err })
+        }
+    }
+
+    return res.json({ success: false, message: 'Not found token.' })
 }
--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,6 @@ const userController = require('../controllers/userController')
 router.get('/', (req, res) => res.send('Hello world'))
 router.get('/api/users', catchErrors(userController.getUsers))
 router.post('/api/authenticate', catchErrors(userController.postAuthenticate))
+router.get('/api/authorize', catchErrors(userController.getAuthorize))
 
 module.exports = router
